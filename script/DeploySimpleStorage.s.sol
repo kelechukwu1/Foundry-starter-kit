4b019bb8d0e54672cfc0620d1978be19942bc273
Kelechukwu <paparazzip8@gmail.com>
2024-11-21 16:54:03 +0000

    first solidity smart contract

---
+++ script/DeploySimpleStorage.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {SimpleStorage} from "../src/SimpleStorage.sol";
+
+// import {console} from "forge-std/console.sol";
+
+contract DeploySimpleStorage is Script {
+    function run() external returns (SimpleStorage) {
+        vm.startBroadcast();
+        SimpleStorage simpleStorage = new SimpleStorage();
+        vm.stopBroadcast();
+        return simpleStorage;
+        // console.log(
+        //     "Deployed SimpleStorage Contract at:",
+        //     address(simpleStorage)
+        // );
+    }
+}
